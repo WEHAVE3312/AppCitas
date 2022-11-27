73865385a85a6618baf1a0bdb01040f091e8f1c1
Kiuber <soporte@thob.kiubix.biz>
2022-11-27 17:52:31 -0600

    Version Final

--- AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
+++ AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
@@ -12,7 +12,11 @@ public static class ApplicationServiceExtensions
     {
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPhotoService, PhotoService>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ILikesRepository, LikesRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<LogUserActivity>(); // Filter
         services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
         services.AddDbContext<DataContext>(options =>
         {
